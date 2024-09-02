2a5abb19e7cbb7ab7de84f95b4149c6d8b949258
Konstantinos K <raistlindragonl@gmail.com>
2024-09-02 16:31:32 +0300

    Implement dark mode and add a color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,15 @@
 import { Heading, HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack padding={2}>
-      <Image src={logo} boxSize={16} />
-      <Heading>Movie Database</Heading>
+    <HStack padding={2} justifyContent="space-between">
+      <HStack>
+        <Image src={logo} boxSize={16} />
+        <Heading>Movie Database</Heading>
+      </HStack>
+      <ColorModeSwitch />
     </HStack>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import theme from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </StrictMode>
