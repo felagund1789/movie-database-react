c54a771dfeef6d5b5a09c9f7d5c96269da8deb43
Konstantinos K <raistlindragonl@gmail.com>
2024-09-03 11:38:35 +0300

    Make NavBar mobile-friendly by moving search input to a new row

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,10 @@
-import { Box, Heading, HStack, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  HStack,
+  Image,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
@@ -8,16 +14,21 @@ interface Props {
 }
 
 const NavBar = ({ onSearch }: Props) => {
+  const multiline = useBreakpointValue({
+    base: true,
+    lg: false,
+  });
+
+  const searchInput = <SearchInput onSearch={onSearch} />;
   return (
     <>
       <HStack padding={2} justifyContent="space-between">
         <Image src={logo} boxSize={12} />
         <Heading>Movie Database</Heading>
-        <Box flex={1}>
-          <SearchInput onSearch={onSearch} />
-        </Box>
+        {!multiline && <Box flex={1}>{searchInput}</Box>}
         <ColorModeSwitch />
       </HStack>
+      {multiline && <Box paddingX={5}>{searchInput}</Box>}
     </>
   );
 };
