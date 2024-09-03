846dbb2cfe846bb8533cdf803291f4f506d4677e
Konstantinos K <raistlindragonl@gmail.com>
2024-09-03 13:20:19 +0300

    Add placeholder image for movies that do not have a poster

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,14 +1,16 @@
 import { Badge, Card, CardBody, Heading, Image } from "@chakra-ui/react";
 import { Movie } from "../types";
+import posterPlaceholder from "../assets/poster-placeholder.png"; 
 
 interface Props {
   movie: Movie;
 }
 
 const MovieCard = ({ movie }: Props) => {
+  const poster = movie.Poster === "N/A" ? posterPlaceholder : movie.Poster;
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image alt={movie.Title} src={movie.Poster} />
+      <Image alt={movie.Title} src={poster} />
       <CardBody>
         <Heading fontSize="xl">{movie.Title} ({movie.Year})</Heading>
         <Badge>{movie.Type}</Badge>
